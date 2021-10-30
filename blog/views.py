1ad504bc19cee3d55f6281f68bf627503afa516e
S. Cetin <nailsolutionsuk@gmail.com>
2021-10-30 12:29:09 +0100

    bootstrap has been added

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ posts = [
         'date_posted': 'August 27, 2018'
     },
     {
-        'author': 'JAne Doe',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'August 28, 2018'
@@ -19,7 +19,7 @@ def home(request):
     context = {
         'posts': posts
     }
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
